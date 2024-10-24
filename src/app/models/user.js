8a0212b3f1d6f3ba2e360be94cb48a2f2feae670
NicholasdeOliveiraSica <nick.sica16@gmail.com>
2024-10-24 15:13:00 -0300

    new commit

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,25 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+
+   static init(sequelize) {
+      super.init(
+         {
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password_hash: Sequelize.STRING,
+            admin: Sequelize.BOOLEAN
+         }, 
+         {
+            sequelize,
+         }
+      );
+   }
+}
+
+export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import User from './app/models/user.js'
+import { v4 } from 'uuid'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    return res.status(200).json({message: "Hello World"})
+routes.get('/create', async (req, res) => {
+
+    const user = await User.create({
+        id: v4(),
+        name: 'Nicholas',
+        email: 'nick@email.com',
+        password_hash: 'h89273hfb892g',
+    })
+
+    return res.status(201).json(user)
 })
 
 export default routes
